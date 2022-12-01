2039d0ae7e674259bd3d6c5802db16df9768a766
Anthony Eden <me@anthonyeden.com>
2022-12-01 08:13:35 +0100

    Read lines from the input file
    Show usage if args length is less than 2

--- day1/p1/src/main.rs
+++ day1/p1/src/main.rs
@@ -1,14 +1,35 @@
 use std::env;
-use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let bin_name = &args[0];
 
+    let args_length = args.len();
+    if args_length < 2 {
+        println!("Usage: {bin_name} file_path");
+        return;
+    }
+    println!("Args length {args_length}");
     let file_path = &args[1];
 
     println!("In file {}", file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Unable to read file {file_path}");
+    if let Ok(lines) = read_lines(file_path) {
+        for line in lines {
+            if let Ok(calories) = line {
+                println!("{calories}");
+            }
+        }
+    }
+}
 
-    println!("Contents:\n{contents}");
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
