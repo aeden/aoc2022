0eff284e6d99044101bfd9aea8a8fd542f605c68
Anthony Eden <me@anthonyeden.com>
2022-12-01 10:46:12 +0100

    Solution to part 2 of day 1

---
+++ day1/p2/src/main.rs
@@ -0,0 +1,54 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let bin_name = &args[0];
+
+    let args_length = args.len();
+    if args_length < 2 {
+        println!("Usage: {bin_name} file_path");
+        return;
+    }
+    println!("Args length {args_length}");
+    let file_path = &args[1];
+
+    println!("In file {}", file_path);
+
+    let mut elves = Vec::new();
+    let mut current = 0;
+
+    if let Ok(lines) = read_lines(file_path) {
+        for line in lines {
+            if let Ok(calories) = line {
+                if calories.is_empty() {
+                    elves.push(current);
+                    current = 0;
+                } else {
+                    let parsed: i32 = calories.parse().unwrap();
+                    current += parsed;
+                }
+            }
+        }
+    }
+
+    elves.sort();
+    let mut elves_rev: Vec<_> = elves.into_iter().rev().collect();
+
+    let _ = elves_rev.split_off(3);
+
+    let sum: i32 = elves_rev.iter().sum();
+    dbg!(elves_rev);
+
+    println!("Sum of top 3: {sum}");
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
